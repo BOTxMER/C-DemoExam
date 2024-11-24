b3e3cba248df638190bfe3b86109d21b393e0149
Ваня <divan-19@mail.ru>
2024-11-24 19:14:20 +0500

    Создание сущностей заявки со всеми параметрами

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,7 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
- 
+
+class Order
+{
+    public int Number { get; set;}
+    public DateOnly StartDate { get; set;}
+    public string Device { get; set;}
+    public string ProblemType { get; set;}
+    public string Description { get; set;}
+    public string CLient { get; set;}
+    public string Status {get; set;}
+    public DateOnly? EndDate { get; set;}=null;
+    public string? Master { get; set;} = "Не назначен";
+    public List<string>? Comments { get; set;} = [];
+}
