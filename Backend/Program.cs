ff80484484b4d9b9320caf2865e2509ae8583f82
Ваня <divan-19@mail.ru>
2024-11-25 19:48:40 +0500

    Исправление ошибок

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -13,7 +13,7 @@ app.UseCors(option => option
 
 string message = "";
 
-app.MapGet("orders", (int param = 0) => 
+app.MapGet("/orders", (int param = 0) => 
 {
     string buffer = message;
     message="";
@@ -21,8 +21,8 @@ app.MapGet("orders", (int param = 0) =>
         return new { repo = repo.FindAll(x=> x.Number == param), message= buffer};
     return new {repo, message= buffer};
 });
-app.MapGet("create", ([AsParameters] Order dto) =>repo.Add(dto));
-app.MapGet("update", ([AsParameters]UpdateOrderDTO dto) =>
+app.MapGet("/create", ([AsParameters] Order dto) =>repo.Add(dto));
+app.MapGet("/update", ([AsParameters]UpdateOrderDTO dto) =>
 {
     var order = repo.Find(x=>x.Number == dto.Number);
     if(order == null)
